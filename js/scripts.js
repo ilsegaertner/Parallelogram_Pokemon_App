26889c95c082c25550731011c2ea82422866b594
ilsegaertner <joachimpruegl@gmail.com>
2023-04-30 09:58:24 +0200

    folder move

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// let myName = 'Joachim';
+// document.write(myName);
+// myName = 'Hannes';
+// document.write (myName)
+
+// let simpleAddition = 2+2;
+// document.write (simpleAddition);
+
+// let size = 100;
+// let doubleSize = 2*size;
+// document.write (doubleSize);
+
+let size = 100;
+let doubleSize = 2*size;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write (minSize);

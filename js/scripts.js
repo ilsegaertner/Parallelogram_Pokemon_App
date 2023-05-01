bc21c3aa3ec56f2c5a50d590332e8ca83bf29313
ilsegaertner <joachimpruegl@gmail.com>
2023-05-01 12:03:47 +0200

    added number object

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,19 @@ let pokemonList [
   {
     name: 'Butterfree', 
     height: 1.1,
-    type: ['bug', 'flying']
+    type: ['bug', 'flying'],
+    number: 12
   }, 
   {
     name: 'Charizard', 
     height: 1.7,
-    type: ['fire', 'flying']
+    type: ['fire', 'flying'],
+    number: 6
   }, 
   {
     name: 'Jigglypuff', 
     height: 0.5;
-    type: ['fairy', 'normal']
+    type: ['fairy', 'normal'], 
+    number: 39
   }
 ]; 

1d529739cf00d95790cd8151cf6f0859098c6f95
ilsegaertner <joachimpruegl@gmail.com>
2023-05-01 11:56:52 +0200

    pokemonList first entries

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList [
+  {
+    name: 'Butterfree', 
+    height: 1.1,
+    type: ['bug', 'flying']
+  }, 
+  {
+    name: 'Charizard', 
+    height: 1.7,
+    type: ['fire', 'flying']
+  }, 
+  {
+    name: 'Jigglypuff', 
+    height: 0.5;
+    type: ['fairy', 'normal']
+  }
+]; 

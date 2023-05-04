02db3f26b5b64dcf4576c050c5ab87954b63248d
ilsegaertner <joachimpruegl@gmail.com>
2023-05-04 17:03:29 +0200

    Add CSS & JSscript

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,23 @@ let pokemonList = [
   {
     name: 'Butterfree', 
     height: 1.1,
-    type: ['bug', 'flying'],
+    type: ['Bug', 'Flying'],
     number: 12
   }, 
   {
     name: 'Charizard', 
     height: 1.7,
-    type: ['fire', 'flying'],
+    type: ['Fire', 'Flying'],
     number: 6
   }, 
   {
     name: 'Jigglypuff', 
     height: 0.5,
-    type: ['fairy', 'normal'], 
+    type: ['Fairy', 'Normal'], 
     number: 39
   }
-]; 
+];
+  
+for (let i=0; i <pokemonList.length; i++) {
+  document.write (" " + pokemonList[i].name + " " + '(' + 'Height:' + " " + pokemonList[i].height + ')' + " " + '(' + 'Type:' + " " + pokemonList[i].type + ')')
+}

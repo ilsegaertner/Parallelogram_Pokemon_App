d1f76fa08ce63184ed2411a22899a81999a7028d
ilsegaertner <joachimpruegl@gmail.com>
2023-05-06 13:33:29 +0200

    Delete CSS Alternatives

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 let pokemonList = [
   {
     name: 'Butterfree', 
@@ -28,3 +30,27 @@ for (let i=0; i <pokemonList.length; i++) {
 }
 
 }
+
+
+
+
+
+
+
+/*
+
+let person1 = {firstName:'Thomas', lastName:'Bernat', age: 31};
+let person2 = {firstName:'Werner', lastName:'Loter', age: 10};
+let person3 = {firstName:'Martin', lastName:'Sang', age: 67};
+let person4 = {firstName:'Ebi', lastName:'Killes', age: 39};
+let person5 = {firstName:'Azou', lastName:'Zupfert', age: 23};
+let person6 = {firstName:'Ahmed', lastName:'Rissen', age: 45};
+let person7 = {firstName:'Kerstin', lastName:'Langham', age: 14};
+let person8 = {firstName:'Eva', lastName:'Secker', age: 56};
+let person9 = {firstName:'Michael', lastName:'Rotweil', age: 23};
+
+let persons = [person1, person2, person3, person4, person5, person6, person7, person8, person9];
+
+for (let i=0; i < persons.length; i++) {console.log(persons[i]);}
+
+*/

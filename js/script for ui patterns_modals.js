f139f6e3c1e23fc2b3098a588379018a657a7430
Ilse Gaertner <joachimpruegl@gmail.com>
2023-06-05 08:17:44 +0200

    add exercise files for ui patterns

---
+++ js/script for ui patterns_modals.js
@@ -0,0 +1,37 @@
+//script for UI Patterns 
+// modals
+
+function showModal() {
+    let modalContainer = document.querySelector ('#modal-container');
+
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    //Add the new modal content
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+
+
+    modalContainer.classList.add ('is-visible');
+}
+
+document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal ('Modal title', 'This is the modal content');
+});
